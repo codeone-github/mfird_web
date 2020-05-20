a78d2955522d835f31010f110780a0dfdf4615de
IhtishamMushtaq <musaibmushtaqq@gmail.com>
2020-05-20 14:01:35 +0530

    final tweaks done and ready to accept bug reports

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function hit() {
     flag=flag+1;
     if(flag%2==0){
         document.getElementsByClassName("menu-icon")[0].setAttribute("class","menu-icon-new");
+        
         document.getElementsByClassName("menu")[0].setAttribute("class","menu-new");
     }
     else{
@@ -10,4 +11,19 @@ function hit() {
         document.getElementsByClassName("menu-new")[0].setAttribute("class","menu");
     }
 }
+
+   var focus_flag=0;
+   function focus_hit() {
+        focus_flag=1;
+        document.getElementsByClassName("menu-icon")[0].setAttribute("class","menu-icon-focussed");
+    
+   if(focus_flag==1){
+               setTimeout(revert,1000);
+    }
+
+    function revert() {
+        document.getElementsByClassName("menu-icon-focussed")[0].setAttribute("class","menu-icon");
+        focus_flag=0;
+    }
+   }
 
